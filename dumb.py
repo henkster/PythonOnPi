e6b320fae37e333e37badf448d6b60ae85e0d0dc
Steve Henke <steve@henkster.com>
2015-11-29 21:42:03 -0600

    First file with Sam.

---
+++ dumb.py
@@ -0,0 +1,9 @@
+y = ["a", "b", "c"]
+for x in y:
+  print(x)
+first = input("what is first? ")
+second = input("what is second? ")
+turd = input("what is turd? ")
+z = [first, second, turd]
+for xx in z:
+  print(xx)
